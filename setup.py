143c077ebdeb8d5cccffeb091acb9ae77944fcc7
Jerad Meisner <jerad.meisner@gmail.com>
2018-02-11 07:03:03 -0800

    removed _init_.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='pysabnzbd',
     packages=['pysabnzbd'],
-    version='0.0.1',
+    version='0.0.2',
     description='Python wrapper for SABnzbd API',
     author='Jerad Meisner',
     author_email='jerad.meisner@gmail.com',
     url='https://github.com/jeradM/pysabnzbd',
-    download_url='https://github.com/jeradM/pysabnzbd/archive/0.0.1.tar.gz',
+    download_url='https://github.com/jeradM/pysabnzbd/archive/0.0.2.tar.gz',
     keywords=['SABnzbd'],
     install_requires=[
         'aiohttp'
