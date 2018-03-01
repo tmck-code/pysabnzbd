0e908c8570c8b08d76881ceae2ead6985670e69b
Jerad Meisner <jerad.meisner@gmail.com>
2018-02-28 22:02:13 -0800

    refactored api methods and added more robust error handling. bumped version to 1.0.0

--- pysabnzbd/__init__.py
+++ pysabnzbd/__init__.py
@@ -1,120 +1,99 @@
-import asyncio
 import aiohttp
 
 
 class SabnzbdApi(object):
-    def __init__(self, base_url, api_key):
+    def __init__(self, base_url, api_key, web_root=''):
         if base_url.endswith('/'):
             base_url = base_url[:-1]
-        self.base_url = base_url
-        self.api_key = api_key
+
+        web_root = web_root.strip('/') + '/'
+
+        self.api_url = '{}/{}{}'.format(base_url, web_root, 'api')
         self.queue = {}
+        self.default_params = {'apikey': api_key, 'output': 'json'}
 
-    @asyncio.coroutine
-    def refresh_queue(self):
+    async def call(self, params):
+        api_params = {**self.default_params, **params}
         try:
-            api_args = {
-                'apikey': self.api_key,
-                'mode': 'queue',
-                'start': '0',
-                'limit': '10',
-                'output': 'json'
-            }
-
-            url = '{}/{}'.format(self.base_url, 'api')
             with aiohttp.ClientSession() as session:
-                resp = yield from session.get(url, params=api_args)
-                json_resp = yield from resp.json()
-                self.queue = json_resp.get('queue')
-        except aiohttp.ClientError:
-            raise SabnzbdApiException(
-                "Failed to communicate with SABnzbd API.")
-
-    @asyncio.coroutine
-    def pause_queue(self):
-        try:
-            api_args = {
-                'apikey': self.api_key,
-                'mode': 'pause',
-                'output': 'json'
-            }
-            url = '{}/{}'.format(self.base_url, 'api')
-            with aiohttp.ClientSession() as session:
-                resp = yield from session.get(url, params=api_args)
-                json_resp = yield from resp.json()
-                if json_resp.get('status', False):
-                    return True
+                resp = await session.get(self.api_url, params=api_params)
+                data = await resp.json()
+                if data.get('status', True) is False:
+                    self.handle_error(data, api_params)
                 else:
-                    return False
+                    return data
         except aiohttp.ClientError:
-            raise SabnzbdApiException(
-                "Failed to communicate with SABnzbd API.")
+            raise SabnzbdApiException('Unable to communicate with Sabnzbd API')
 
-    @asyncio.coroutine
-    def resume_queue(self):
-        try:
-            api_args = {
-                'apikey': self.api_key,
-                'mode': 'resume',
-                'output': 'json'
-            }
-            url = '{}/{}'.format(self.base_url, 'api')
-            with aiohttp.ClientSession() as session:
-                resp = yield from session.get(url, params=api_args)
-                json_resp = yield from resp.json()
-                if json_resp.get('status', False):
-                    return True
-                else:
-                    return False
-        except aiohttp.ClientError:
-            raise SabnzbdApiException(
-                "Failed to communicate with SABnzbd API.")
+    async def refresh_data(self):
+        queue = await self.get_queue()
+        history = await self.get_history()
+        totals = {}
+        for k in history:
+            if k[-4:] == 'size':
+                totals[k] = self.convert_size(history.get(k))
+        self.queue = {**totals, **queue}
 
-    @asyncio.coroutine
-    def set_speed_limit(self, speed=100):
-        try:
-            api_args = {
-                'apikey': self.api_key,
-                'mode': 'config',
-                'name': 'speedlimit',
-                'value': speed,
-                'output': 'json'
-            }
-            url = '{}/{}'.format(self.base_url, 'api')
-            with aiohttp.ClientSession() as session:
-                resp = yield from session.get(url, params=api_args)
-                json_resp = yield from resp.json()
-                if json_resp.get('status', False):
-                    return True
-                else:
-                    return False
-        except aiohttp.ClientError:
-            raise SabnzbdApiException(
-                "Failed to communicate with SABnzbd API.")
+    async def get_history(self):
+        params = {'mode': 'history', 'limit': 1}
+        history = await self.call(params)
+        return history.get('history')
 
-    @asyncio.coroutine
-    def check_available(self):
-        try:
-            api_args = {
-                'apikey': self.api_key,
-                'mode': 'qstatus',
-                'output': 'json'
-            }
+    async def get_queue(self):
+        params = {'mode': 'queue', 'start': '0', 'limit': '10'}
+        queue = await self.call(params)
+        return queue.get('queue')
 
-            url = '{}/{}'.format(self.base_url, 'api')
-            with aiohttp.ClientSession() as session:
-                resp = yield from session.get(url, params=api_args)
-                json_obj = yield from resp.json()
-            if not json_obj.get('status', True):
-                raise SabnzbdApiException(
-                    json_obj.get('error',
-                                 'Failed to communicate with SABnzbd API.'))
-        except aiohttp.ClientError:
-            raise SabnzbdApiException(
-                "Failed to communicate with SABnzbd API.")
+    async def pause_queue(self):
+        params = {'mode': 'pause'}
+        await self.call(params)
 
+    async def resume_queue(self):
+        params = {'mode': 'resume'}
+        await self.call(params)
+
+    async def set_speed_limit(self, speed=100):
+        params = {'mode': 'config', 'name': 'speedlimit', 'value': speed}
+        await self.call(params)
+
+    async def check_available(self):
+        params = {'mode': 'queue'}
+        await self.call(params)
         return True
+
+    def convert_size(self, size_str):
+        suffix = size_str[-1]
+        if suffix == 'K':
+            multiplier = 1.0 / (1024.0 * 1024.0)
+        elif suffix == 'M':
+            multiplier = 1.0 / 1024.0
+        elif suffix == 'T':
+            multiplier = 1024.0
+        else:
+            multiplier = 1
+
+        try:
+            val = float(size_str.split(' ')[0])
+            return val * multiplier
+        except ValueError:
+            return 0.0
+
+    def handle_error(self, data, params):
+        error = data.get('error', 'API call failed')
+        mode = params.get('mode')
+        raise SabnzbdApiException(error, mode=mode)
 
 
 class SabnzbdApiException(Exception):
-    pass
+    def __init__(self, message, mode=None):
+        self.message = message
+        self.mode = mode
+
+    def __str__(self):
+        if self.mode is not None:
+            msg_format = '{}: calling api endpoint \'{}\''
+        else:
+            msg_format = '{}'
+        return msg_format.format(self.message,
+                                 self.mode if self.mode is not None else '')
+
