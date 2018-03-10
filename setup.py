da49d450d3de3712e2a0b0e3589af13889a0a12b
Jerad Meisner <jerad.meisner@gmail.com>
2018-03-09 23:13:39 -0800

    bump pypi version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='pysabnzbd',
     packages=['pysabnzbd'],
-    version='1.0.0',
+    version='1.0.1',
     description='Python wrapper for SABnzbd API',
     author='Jerad Meisner',
     author_email='jerad.meisner@gmail.com',
     url='https://github.com/jeradM/pysabnzbd',
-    download_url='https://github.com/jeradM/pysabnzbd/archive/1.0.0.tar.gz',
+    download_url='https://github.com/jeradM/pysabnzbd/archive/1.0.1.tar.gz',
     keywords=['SABnzbd'],
     install_requires=[
         'aiohttp'
